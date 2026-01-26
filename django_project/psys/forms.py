2c11d1c35d8b7a415ea2cb49f3a914af44c5a616
ibuki1204 <ktc24a32j0003@edu,kyoto-tech.ac.jp>
2026-01-26 21:47:56 +0900

    forms書き換え

--- django_project/psys/forms.py
+++ django_project/psys/forms.py
@@ -4,4 +4,13 @@ from .models import Customer
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ["customer_name", "email"]
+        fields = [
+            "customer_code",
+            "customer_name",
+            "customer_telno",
+            "customer_postalcode",
+            "customer_address",
+            "discount_rate",
+            "delete_flag",
+        ]
+
